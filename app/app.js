e3e613dc4e9e081095754fbe6f708f1c952e550c
Ownerek <mat.waligora@gmail.com>
2015-07-16 14:11:53 +0200

    Working game engine

--- app/app.js
+++ app/app.js
@@ -17,26 +17,56 @@ var battleships = (function(){
         ships : 0,
         canShoot : false
     };
-    //create enemy map for players
+    //create enemy map for players and assign them with null value
     var player1Enemy = {
-        enemyMap : new GenerateRoom(10, 10, '1', true)
+        enemyMap :  null
     };
     var player2Enemy = {
-        enemyMap : new GenerateRoom(10, 10, '2', true)
+        enemyMap : null
+    };
+    //game state object
+    var gameState = {
+        running : false,
+        generateMaps : function(){
+            player1Enemy.enemyMap = new GenerateRoom(10, 10, '1', true);
+            player2Enemy.enemyMap = new GenerateRoom(10, 10, '2', true);
+        }
     };
 
 
-
-    function shoot(x, y, shooter, target){
-        if(target.map[x][y].shipPresent){
-            console.log(shooter + " hit the target");
+    function shoot(x, y, shooter, target, element){
+        var tgt = target.name + "_" + x + "x" + y;
+        var tgtField = document.getElementById(tgt);
+        if(shooter.canShoot){
+            if(target.map[x][y].shipPresent){
+                console.log(shooter.name + " hit the target");
+                target.map[x][y].shipPresent = 'destroyed';
+                target.ships--;
+                tgtField.style.backgroundColor = 'pink';
+            } else{
+                console.log(shooter.name + " missed the target");
+                tgtField.style.backgroundColor = 'gray';
+            }
+            shooter.canShoot = false;
+            target.canShoot = true;
+            element.style.backgroundColor = 'red';
+            element.removeEventListener('click', arguments.callee); // remove event listener
         } else{
-           console.log(shooter + " missed the target");
+            console.log("it's not your turn");
         }
-        shooter.canShoot = false;
-        target.canShoot = true;
+
+        gameState = checkShips(target);
+        if(!gameState){
+            alert(shooter.name + " WINS!");
+            setTimeout(location.reload(), 2000);
+
+        }
+
     }
 
+    function checkShips(player){
+        return player.ships > 0 ? true : false;
+    }
 
     //Game map generations
     function GenerateRoom(x, y, playerId, enemy){
@@ -47,8 +77,8 @@ var battleships = (function(){
         var player = document.getElementById(ene);
         var map = document.createElement('div');
         map.className = ene;
-        player.appendChild(map);
         playerSide.appendChild(player);
+        player.appendChild(map);
 
         for(var i = 0; i < x; i++){
             for(var j = 0; j < y; j++){
@@ -66,53 +96,58 @@ var battleships = (function(){
                 }
                 map.appendChild(div);
             }
-            room.push([]);
+            room.push([]); //add next row
         }
         return room;
     }
     //add click event listener function
     function addListener(element){
         element.addEventListener('click', function(){
-            element.style.backgroundColor = 'red';
-            this.removeEventListener('click', arguments.callee);
-            console.log('clicked on: ' + element.id);
+            var player = element.parentNode.className === 'en_1' ? 'pl_1' : 'pl_2';
+            var x = Number(element.id.charAt(5));
+            var y = Number(element.id.charAt(7));
+            console.log(x + '___' + y);
+            if(player === player1.name){
+                shoot(x, y, player1, player2, element);
+            } else{
+                shoot(x, y, player2, player1, element);
+            }
+            console.log(player);
+
         })
     }
 
     function addShip(element){
         element.addEventListener('click', function(){
             var player = element.parentNode.className;
-
+            var x = Number(element.id.charAt(5));
+            var y = Number(element.id.charAt(7));
             if(player === player1.name){
-                if(player1.ships < 15){
+                if(player1.ships < 5){
                     player1.ships++;
-                    console.log(player1.name + " has: " + player1.ships);
+                    player1.map[x][y].shipPresent = true;
+                    element.style.backgroundColor = 'blue';
+                    console.log(player1.name + " has: " + player1.ships + " ships");
                 } else{
                     console.log("Maximum ships reached");
                 }
             } else{
-                if(player2.ships < 15){
+                if(player2.ships < 5){
                     player2.ships++;
-                    console.log(player2.name + " has: " + player2.ships);
+                    player2.map[x][y].shipPresent = true;
+                    element.style.backgroundColor = 'blue';
+                    console.log(player2.name + " has: " + player2.ships + " ships");
                 } else{
                     console.log("Maximum ships reached");
                 }
             }
-
-            element.style.backgroundColor = 'blue';
-            this.removeEventListener('click', arguments.callee);
-            console.log('clicked on: ' + element.id);
-
-
+            if(player1.ships === 5 && player2.ships === 5 && gameState.running === false){
+                gameState.running = true;
+                gameState.generateMaps();
+            }
+            this.removeEventListener('click', arguments.callee); // remove event listener
         })
     }
-
-    function test(){
-        player2.map[1][1].shipPresent = true;
-        console.log(player2.map[1][1]);
-        player1.shoot(1,1, player1.name, player2);
-    }
-    test();
 });
 
 //instantiate game
